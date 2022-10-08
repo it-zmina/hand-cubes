e5f926080c7efa28d501d0fdff3b121c028b6914
Alex <userok.zmina@gmail.com>
2022-10-09 01:17:15 +0300

    Add demo scene

---
+++ src/app.js
@@ -0,0 +1,133 @@
+import * as THREE from 'three'
+import {VRButton} from "three/examples/jsm/webxr/VRButton"
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader"
+import {XRControllerModelFactory} from "three/examples/jsm/webxr/XRControllerModelFactory";
+
+import blimp from "../assets/Blimp.glb"
+
+class App {
+  constructor() {
+    const container = document.createElement('div')
+    document.body.appendChild(container)
+
+    this.camera = new THREE.PerspectiveCamera(50,
+        window.innerWidth / window.innerHeight, 0.1, 200)
+    this.camera.position.set(0, 1.6, 3)
+
+    this.scene = new THREE.Scene()
+    this.scene.background = new THREE.Color(0x505050)
+
+    const ambient = new THREE.HemisphereLight(0x606060, 0x404040, 1)
+    this.scene.add(ambient)
+
+    const light = new THREE.DirectionalLight(0xffffff)
+    light.position.set(1, 1, 1).normalize()
+    this.scene.add(light)
+
+    this.renderer = new THREE.WebGLRenderer({antialias: true})
+    this.renderer.setPixelRatio(window.devicePixelRatio)
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.outputEncoding = THREE.sRGBEncoding
+    container.appendChild(this.renderer.domElement)
+
+
+    this.initScene()
+    this.setupVR()
+
+    this.renderer.setAnimationLoop(this.render.bind(this))
+    window.addEventListener('resize', this.resize.bind(this))
+  }
+
+
+  initScene() {
+    const geometry = new THREE.BoxBufferGeometry(.5, .5, .5)
+    const material = new THREE.MeshStandardMaterial({color: 0xFF0000})
+    this.mesh = new THREE.Mesh(geometry, material)
+    this.scene.add(this.mesh)
+
+    const geometrySphere = new THREE.SphereGeometry(.7, 32, 16)
+    const materialSphere = new THREE.MeshBasicMaterial({color: 0xffff00})
+    const sphere = new THREE.Mesh(geometrySphere, materialSphere)
+    this.scene.add(sphere)
+
+    sphere.position.set(1.5, 0, 0)
+
+    this.loadAsset(blimp, -.5, .5, 1, scene => {
+      const scale = 5
+      scene.scale.set(scale, scale, scale)
+      self.blimp = scene
+    })
+
+  }
+
+  loadAsset(gltfFilename, x, y, z, sceneHandler) {
+    const self = this
+    const loader = new GLTFLoader()
+    // Provide a DRACOLoader instance to decode compressed mesh data
+    const draco = new DRACOLoader()
+    draco.setDecoderPath('draco/')
+    loader.setDRACOLoader(draco)
+
+    loader.load(gltfFilename, (gltf) => {
+          const gltfScene = gltf.scene
+          self.scene.add(gltfScene)
+          gltfScene.position.set(x, y, z)
+          if (sceneHandler) {
+            sceneHandler(gltfScene)
+          }
+        },
+        null,
+        (error) => console.error(`An error happened: ${error}`)
+    )
+  }
+
+  setupVR() {
+    this.renderer.xr.enabled = true
+    document.body.appendChild(VRButton.createButton(this.renderer))
+
+    // Add left grip controller
+    const gripL = this.renderer.xr.getControllerGrip(0)
+    gripL.add(new XRControllerModelFactory().createControllerModel(gripL))
+    this.scene.add(gripL)
+
+    // Add right grip controller
+    const gripR = this.renderer.xr.getControllerGrip(1)
+    gripR.add(new XRControllerModelFactory().createControllerModel(gripR))
+    this.scene.add(gripR)
+
+    // Add beams
+    const geometry = new THREE.BufferGeometry()
+        .setFromPoints([
+            new THREE.Vector3(0,0,0),
+            new THREE.Vector3(0,0,-1)
+        ])
+    const line = new THREE.Line(geometry)
+    line.name = 'line'
+    line.scale.z = 5
+
+    gripL.add(line.clone())
+    gripR.add(line.clone())
+  }
+
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight
+    this.camera.updateProjectionMatrix()
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+  }
+
+  render() {
+    if (this.mesh) {
+      this.mesh.rotateX(0.005)
+      this.mesh.rotateY(0.01)
+    }
+
+    if (this.blimp) {
+      this.blimp.rotateY(0.1 * xAxis)
+      this.blimp.translateY(.02 * yAxis)
+    }
+    this.renderer.render(this.scene, this.camera)
+  }
+}
+
+export {App}
